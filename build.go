4b57d6d51075192b033e5bb13d3f55f979807a86
Andrew Bates <a@sprock.io>
2025-11-12 14:17:44 -0600

    Remove unused files

--- build.go
+++
@@ -1,4 +0,0 @@
-package nevrcap
-
-//go:generate protoc -I./proto -I. --go_out=./gen/go --go_opt=paths=source_relative rtapi/telemetry_v1.proto
-//go:generate protoc -I./proto -I. --go_out=./gen/go --go_opt=paths=source_relative apigame/http_v1.proto
